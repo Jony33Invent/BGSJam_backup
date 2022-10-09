dc38036a01c09d40b358d82247718aa2dc6fedda
Jony33Invent <35571614+Jony33Invent@users.noreply.github.com>
2022-10-09 13:37:10 -0300

    Final fINAL

--- BGS Jam/Assets/CircleRenderer.cs
+++ BGS Jam/Assets/CircleRenderer.cs
@@ -8,7 +8,7 @@ public class CircleRenderer : MonoBehaviour
     [SerializeField] private int circleSteps;
     [SerializeField] private float circleRadius;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         DrawCircle(circleSteps, circleRadius);
     }
